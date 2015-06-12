9670311c23e1be1f80ad993432ec7fa833cf301c
Arvind Naidu <anaidu@mobiquityinc.com>
2015-06-12 15:51:29 -0700

    test

--- server/server.js
+++ server/server.js
@@ -1,16 +1,22 @@
-var WebSocketServer = require('ws').Server;
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
 
-var wss = new WebSocketServer({port: process.env.PORT || 5000});
+app.use(express.static(__dirname + "/"))
 
-wss.broadcast = function(data) {
-    for(var i in this.clients) {
-        this.clients[i].send(data);
-    }
-};
+var server = http.createServer(app)
+server.listen(port)
 
-wss.on('connection', function(ws) {
+console.log("http server listening on %d", port)
+
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
+
+wss.on("connection", function(ws) {
     ws.on('message', function(message) {
         console.log('received: %s', message);
         wss.broadcast(message);
     });
-});
+})
